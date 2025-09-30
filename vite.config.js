a794af90abc9de495d20aca4317b15f2dcb093ef
Lukas656 <le.lu.ka2018@gmail.com>
2025-09-29 22:12:35 -0300

    V1 prototipo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ducSpace/',
   plugins: [react(), tailwindcss(),],
   server: {
     host: true, // expõe para a rede
