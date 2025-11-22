96b7b54accb5c9880738d732371eca64caa9681e
Lukas656 <le.lu.ka2018@gmail.com>
2025-11-22 14:33:10 -0300

    Fiz error render

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,26 +4,11 @@ import About from "../components/About";
 import Cardapio from "../components/Cardapio";
 import Footer from "../components/Footer";
 import Testimonials from "../components/Testimonials";
-const WHATSAPP = import.meta.env.VITE_WHATSAPP_REDIRECT;
-
-import {
-  MessageSquare, 
-} from "lucide-react";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-       <a
-        href={WHATSAPP}
-        aria-label={`Acessar whatsapp`}
-        target={"_blank"}
-        rel={"noopener noreferrer"}
-        className="fixed bottom-6 right-6 p-4 rounded-full shadow-lg z-50"
-        style={{ background: "#25D366" }}
-      >
-        {MessageSquare}
-      </a>
       <About />
       <Cardapio />
       <Testimonials />
